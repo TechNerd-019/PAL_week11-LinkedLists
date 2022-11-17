55688c026a31c3ab758895b2a695404b56f4870d
Salah Salame <Ssalame7167@conestogac.on.ca>
2022-11-16 19:32:00 -0500

    Added delete and display list functions.

--- LinkedLists/McDonalds.h
+++ LinkedLists/McDonalds.h
@@ -6,4 +6,5 @@ typedef struct clientinfo {
 	int age;					// First element on the list.
 	struct listnode* next;		// Declaring the pointer to our link list.
 
+
 }LISTNODE, *PLISTNODE;
--- LinkedLists/Source.c
+++ LinkedLists/Source.c
@@ -6,24 +6,29 @@
 // PAL. Monday, Nov. 14, 2022.
 
 void addItem(PLISTNODE*, int);
+void deleteItem(PLISTNODE*, int);
+void printList(PLISTNODE*);
 
 int main(void) {
-
 	int var;
-
 	PLISTNODE list = NULL;	// There is nothing on our list. List is pointing to NULL.
 							// When adding to the list, the new item will point to NULL.
-
 	puts("Enter a number: ");
 	scanf_s("%d", &var);
+	addItem(&list, var);
+	printList(list);
 
-	addItem(list, var);
+	puts("Enter node number you want to delete: ");
+	scanf_s("%d", &var);
+	deleteItem(&list, var);
+	printList(list);
+
+	return 0;
 
 }
 
 void addItem(PLISTNODE *list, int var) {
-
-	if (list == NULL) {	
+	if (*list == NULL) {	
 
 		PLISTNODE newNode = NULL;		// Initializing a new node into our list.
 		newNode = (PLISTNODE)malloc(sizeof(LISTNODE));		//Assigning memory in the heap.
@@ -34,13 +39,54 @@ void addItem(PLISTNODE *list, int var) {
 		*list = newNode;
 	}
 	else {
-
 		PLISTNODE current = *list;
 		while (list != NULL) {
-
 			current = current->next;
-			
 		}
-
+		PLISTNODE newNode = NULL;		// Initializing a new node into our list.
+		newNode = (PLISTNODE)malloc(sizeof(LISTNODE));		//Assigning memory in the heap.
+		newNode->age = var;				// The new node will be assigning the items of the list.
+		newNode->next = NULL;			// The new node will point to NULL.
+		current->next = newNode;
+										// This means that the newItem will now point to NULL.
+										// So the *list is now pointing to the newItem.
+		*list = newNode;
 	}
+}
+
+void deleteItem(PLISTNODE *list, int var) {
+
+	PLISTNODE current = *list;
+
+	if (current->age == var) {
+		if (current->next != NULL) {
+
+			*list = current->next;
+		}
+		else {
+			*list = NULL;
+		}
+	}
+	else {
+		return;
+	}
+	
+	free(current);
+	return;
+}
+
+void printList(PLISTNODE* list) {
+
+	PLISTNODE current = list;
+	if (!current)	// list is empty, don't print!
+		return;
+
+	do
+	{
+		printf("%d ", current->age);
+		current = current->next;
+	} while (current != NULL);
+
+	printf("\n");
+
 }
